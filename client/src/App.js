d4b6f487e7ce3a54fb9336196df5140d317df385
Anya <hannakilpatrick@gmail.com>
2018-06-26 20:22:00 -0400

    modified routes path

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import SignUp from './pages/SignUp';
+import Profile from "./pages/Profile";
 
 const App = () => (
  <Router>
     <div className="wrap">
       <Route exact path="/" component={Login}/>
-      <Route exact path="/signup" component={SignUp}/>
+      <Route exact path="/app/signup" component={SignUp}/>
+      <Route exact path="/app/profile" component={Profile}/>
     </div>
   </Router>
 )
