c95107479c0e2cb82ff3a07d907228a241b96db4
Anya <hannakilpatrick@gmail.com>
2018-06-30 23:52:25 -0400

    commented out for now

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,14 @@
-const db = require("../models");
-
-
-// Defining methods for the userController
-module.exports = {
-    create: function(req, res){
-        db.User.create(req.body)
-        .then(dbModel=>res.json(dbModel))
-        .catch(err=>res.status(423).json(err))
-    },
-    logout: function(req, res) {
-        req.logout();
-        res.redirect('/');
-    }
-};
+// const db = require("../models");
+// // Defining methods for the userController
+// module.exports = {
+//     // create: function(req, res){
+//     //     db.User.create(req.body)
+//     //     .then(dbModel=>res.json(dbModel))
+//     //     .catch(err=>res.status(423).json(err))
+//     // }
+//     searchUsers: function(req, res){
+//         db.User.find({country:req.params.country})
+//         .then(users=>res.json(users))
+//         .catch(err=>res.status(423).json(err))
+//     }
+// };
