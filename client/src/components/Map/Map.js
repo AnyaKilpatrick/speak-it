2b240eace60eba55a25263ecc4a88d0cab0b1b16
Anya <hannakilpatrick@gmail.com>
2018-06-30 23:51:16 -0400

    displaying labels over countries

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,45 +1,65 @@
 import React, {Component} from "react";
 import { VectorMap } from "react-jvectormap";
-
+import "./Map.css";
+import Tooltip from '@material-ui/core/Tooltip';
 
 class Map extends Component {
+
     render(){
         return(
-            <div style={{width: 600, height: 600}}>
+            // <div style={{width: 800, height: 800, marginTop:-170}}>
                 <VectorMap map={"world_mill"}
                     backgroundColor="#ffffff00"
                     ref="map"
+                    containerClassName="map"
                     containerStyle={{
                         width: '100%',
-                        height: '100%'
+                        height: '100%',
+                        marginTop: -120
                     }}
                     regionStyle= {{
                         initial: {
-                        fill: '#128da7'
+                        fill: 'rgba(155, 153, 153, 0.897)'
                         },
                         hover: {
                             fill: "#A0D1DC"
                         },
-                        // selected: {
-                        //     fill: 'yellow'
-                        // }
-                        // series: {
-                        //     regions: [{
-                        //       values: gdpData,
-                        //       scale: ['#C8EEFF', '#0071A4'],
-                        //       normalizeFunction: 'polynomial'
-                        //     }]
-                        //   },
-                        //   onRegionTipShow: function(e, el, code){
-                        //     el.html(el.html()+' (GDP - '+gdpData[code]+')');
-                        //   }
+                        selected: {
+                            fill: 'yellow'
+                        }
                     }}
-                    containerClassName="map"
+                    regionLabelStyle={{
+                        initial: {
+                            'font-family': 'Verdana',
+                            'font-size': '12',
+                            'font-weight': 'bold',
+                            cursor: 'default',
+                            fill: 'black'
+                        },
+                        hover: {
+                            cursor: 'pointer'
+                        },
+                        onLabelShow: function(event, label, code){
+                            label.text('bla bla bal');
+                        }
+                    }}
+                    onRegionTipShow={ function(e, el, code){
+                        // el.html(el.html(el[0].innerHTML));
+                        // console.log(el[0]);
+                        el[0].style.zIndex = 1000;
+                    }}
+                    // onRegionLabelShow={ function(event, label, code){
+                    //     // el.html(el.html(el[0].innerHTML));
+                    //     label.html(""+label.html());
+                    //     console.log(code);
+                    // }}
+
                 />
-            </div>
+            // </div>
         )
     }
 }
+
 
 export default Map;
 
