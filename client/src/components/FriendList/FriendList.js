d3eab4c3e86ce2742727825c82f8aeb44ae96e8b
Anya <hannakilpatrick@gmail.com>
2018-07-14 01:46:51 -0400

    modified code to improve the way messages are displayed

--- client/src/components/FriendList/FriendList.js
+++ client/src/components/FriendList/FriendList.js
@@ -1,21 +1,20 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
+
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+
+
+
+
+
 import FolderIcon from '@material-ui/icons/Folder';
-import DeleteIcon from '@material-ui/icons/Delete';
+
 import Icon from '@material-ui/core/Icon';
 import Tooltip from '@material-ui/core/Tooltip';
 
@@ -45,23 +44,14 @@ const styles = theme => ({
   }
 });
 
-// function generate(element) {
-//   return [0, 1, 2].map(value =>
-//     React.cloneElement(element, {
-//       key: value,
-//     }),
-//   );
-// }
-
 class FriendList extends Component {
   state = {
-    dense: false,
     secondary: false,
   };
 
   render() {
     const { classes } = this.props;
-    const { dense, secondary } = this.state;
+    const { secondary } = this.state;
 
     return (
         <ListItem 
