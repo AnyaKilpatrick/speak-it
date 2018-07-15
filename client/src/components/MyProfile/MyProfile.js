40989115cd024c066f9c555b78c52c2c8fe8b644
Anya <hannakilpatrick@gmail.com>
2018-07-15 19:10:48 -0400

    responsive profile page

--- client/src/components/MyProfile/MyProfile.js
+++ client/src/components/MyProfile/MyProfile.js
@@ -28,14 +28,33 @@ const styles = theme => ({
     },
     userInfoDiv:{
       // backgroundColor:"rgba(255, 255, 255, 0.299)",
-      borderRadius: 25,
+      // borderRadius: 25,
       // backgroundImage: "url('./images/plane.jpg')",
-      backgroundColor: "rgba(0, 0, 0, 0.168)",
+      marginTop:40,
+      // backgroundColor: "rgba(0, 0, 0, 0.168)",
       marginTop:-100,
-      height: "100vh",
+      height: "100%",
       border:0,
       marginLeft:0,
       marginRight:0
+    },
+    userName:{
+      fontFamily:"'Cabin Sketch', cursive",
+      color:"#4d4d62"
+    },
+    userInfo:{
+      fontFamily:"'Cabin Sketch', cursive",
+      color:"#3a3a41"
+    },
+    infoDiv:{
+      [theme.breakpoints.down('sm')]: {
+        marginTop: 90,
+        marginLeft:30
+      },
+      [theme.breakpoints.down('xs')]: {
+        marginTop: 20,
+        marginLeft:"20%"
+      },
     }
     // avatar: {
     //   margin: 10,
@@ -76,33 +95,31 @@ class MyProfile extends Component {
     }
 
     render(){
-        // const { classes } = this.props.classes;
+        const {classes} = this.props;
 
         return(
-            <Grid container spacing={16}>
-              <Grid item xs={9} >
+            <Grid container direction="row" spacing={16}>
+                <Grid item xs={12} sm={12} md={3} lg={3} xl={3} className={this.props.classes.userInfoDiv}>
+                  <Grid container direction="row" spacing={16} justify="center">
+                    <Grid item xs={8} sm={4} md={10} lg={10} xl={10}>
+                      {/* <Avatar alt="Remy Sharp" src="http://www.geonames.org/flags/x/af.gif" className={this.props.classes.avatar} /> */}
+                      <img id="myAvatar" src={this.state.imageSrc}/>
+                    </Grid>
+                    <Grid className={classes.infoDiv} item xs={12} sm={6} md={10} lg={10} xl={10}>
+                      <h1 className={classes.userName}>{this.props.fullname}</h1>
+                      <h3 className={classes.userInfo}>Country: {this.props.country}</h3>
+                      <h3 className={classes.userInfo}>Native language: {this.props.nativeLang}</h3>
+                      <h3 className={classes.userInfo}>About me: {this.props.about}</h3>
+                    </Grid>
+                  </Grid>
+              </Grid>
+              <Grid item xs={12} sm={12} md={9} lg={9} xl={9} >
                 <Grid container spacing={16} justify="center">
-                  <Grid item xs={12} className={this.props.classes.mapDiv}>
+                  <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={this.props.classes.mapDiv}>
                     {this.props.children}
                   </Grid>
                 </Grid>
               </Grid>
-              <Grid item xs={3} className={this.props.classes.userInfoDiv}>
-                  <Grid container spacing={16} justify="center">
-                    <Grid item xs={10}>
-                      {/* <Avatar alt="Remy Sharp" src="http://www.geonames.org/flags/x/af.gif" className={this.props.classes.avatar} /> */}
-                      <img id="myAvatar" src={this.state.imageSrc}/>
-                    </Grid>
-                    <Grid item xs={10}>
-                      <h1>{this.props.fullname}</h1>
-                      <h3>Country: {this.props.country}</h3>
-                      <h3>Native language: {this.props.nativeLang}</h3>
-                      <h3>About me: {this.props.about}</h3>
-                    </Grid>
-                  </Grid>
-              </Grid>
-
-
             </Grid>
         )
     }
