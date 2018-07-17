588c3f8b151f9bbd85edf0e18b499280d1495fe2
Anya <hannakilpatrick@gmail.com>
2018-07-17 02:44:40 -0400

    socket.io issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = require("socket.io")();
 // const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const socketPort = 3000;
+const socketPort = 8000;
 
 
 //setting up Passport
@@ -61,8 +61,8 @@ app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
 
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
+// var server = require("http").Server(app);
+var io = require("socket.io")(socketPort);
 
 io.on("connection", require("./socket.js"));
 
